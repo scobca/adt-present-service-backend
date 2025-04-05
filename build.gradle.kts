ae1d69b516fa49421978b3ffb9f0599c7a4ac15a
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-04-05 23:25:48 +0300

    build(core): updated project core, convert to Web

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,11 +22,13 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("io.github.cdimascio:dotenv-java:2.2.0")
 }
 
 kotlin {
--- src/main/kotlin/org/itmo/adtpresentcontroller/AdtPresentControllerApplication.kt
+++ src/main/kotlin/org/itmo/adtpresentcontroller/AdtPresentControllerApplication.kt
@@ -2,8 +2,10 @@ package org.itmo.adtpresentcontroller
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories
 class AdtPresentControllerApplication
 
 fun main(args: Array<String>) {
